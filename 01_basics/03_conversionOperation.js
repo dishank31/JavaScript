ae74f6425fd555794f6133950ed1e0c75029174b
Dishank Gandhi <dishankgandhi561@gmail.com>
2026-06-17 15:38:36 +0000

    Some Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,3 +24,37 @@ let stringNumber = String(someNumber)
 
 console.log(typeof stringNumber)
 console.log(stringNumber);
+
+// ****************** Opeartions ******************
+
+let value = 3;
+let negValue = -value
+console.log(negValue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/3)
+console.log(2%3)
+
+let str1 = "Hello"
+let str2 = " Dishank"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+// These type of tricky codes you should avoid
+console.log(+true)
+console.log(+"")
+
+let num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;      // Postfix and Prefix
+++gameCounter
+console.log(gameCounter);
